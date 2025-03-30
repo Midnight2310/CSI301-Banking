11673b83c6c730047358298b75467121a5455cf1
Nine <ninepiyakrit@gmail.com>
2025-03-30 21:21:28 +0700

    1

---
+++ Bank contract/Bank.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Bank {
+    
+    mapping(address => uint) _balances;
+
+    string public studentName = "Piyakrit Wongkasamesak";
+    string public studentID = "65054924";
+
+    event Deposit(address indexed owner, uint amount);
+    event Withdraw(address indexed owner, uint amount);
+
+    function deposit() public payable {
+        require(msg.value > 0, "Deposit money is zero");
+        _balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function withdraw(uint amount) public {
+        require(amount > 0 && amount <= _balances[msg.sender], "Not enough money");
+        payable(msg.sender).transfer(amount);
+        _balances[msg.sender] -= amount;
+        emit Withdraw(msg.sender, amount);
+    }
+
+    function checkBalance() public view returns (uint balance) {
+        return _balances[msg.sender];
+    }
+
+    function getStudentInfo() public view returns (string memory, string memory) {
+        return (studentName, studentID);
+    }
+}
